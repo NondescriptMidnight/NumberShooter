fc8d3f213868710df7a0841e2d11bc3b1cfc955a
Stacey-Eva Fabrè <59258519+NondescriptMidnight@users.noreply.github.com>
2020-01-20 11:53:56 +0200

    Everything for the game
    Basically just uploaded the whole game.

---
+++ Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public AudioClip endBuzzer;
+
+    [SerializeField]
+    public Stat health;
+    public static float speed;
+    private bool hasPlayed = false;
+	// Use this for initialization
+	private void Awake() {
+        health.Initialize();
+	}
+
+    private void Start()
+    {
+        speed = 1f;
+    }
+	
+	// Update is called once per frame
+	public void Update () {
+        health.CurrentVal -= speed * Time.deltaTime;
+        if (health.CurrentVal == 0)
+        {
+            Invoke("GameOver", 3f);
+            if (!hasPlayed)
+            {
+                AudioSource.PlayClipAtPoint(endBuzzer, Camera.main.transform.position, 1f);
+                hasPlayed = true;
+            }
+        }
+    }
+    void GameOver()
+    {
+        Application.LoadLevel(1);
+    }
+}
